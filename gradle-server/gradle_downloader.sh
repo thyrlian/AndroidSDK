9aaf46b289c07a555bdde28005eebdfd8d140848
Jing Li <thyrlian@gmail.com>
2017-10-14 15:04:56 +0200

    Refactor gradle_downloader script

--- gradle-server/gradle_downloader.sh
+++ gradle-server/gradle_downloader.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+check_and_set_parameters() {
+  if [ "$#" -lt 1 ]; then
+    echo "Missing parameter for download directory"
+    exit 1
+  elif [ "$#" -lt 2 ]; then
+    DOWNLOAD_AMOUNT=65535
+  fi
+  DOWNLOAD_DIRECTORY=$1
+  [ -z $DOWNLOAD_AMOUNT ] && DOWNLOAD_AMOUNT=$2
+}
+
 check_installation_of_wget() {
   which wget > /dev/null 2>&1
-  return $?
+  if [ $? -ne 0 ]; then
+    echo "wget is not installed"
+    exit 1
+  fi
 }
 
 download_from_link_to_dir() {
@@ -21,23 +35,15 @@ download_from_link_to_dir() {
 }
 
 parse_gradle_distributions_links_and_download() {
-  local directory=$1
-  local amount=$2
   local url="https://services.gradle.org/distributions/"
   local data=$(wget -q -O- $url | grep -o "\".*distributions.*all\.zip\"" | grep -v "rc\|milestone" | sed "s/\"//g" | sed "s/.*distributions/https:\/\/services.gradle.org\/distributions/g")
   read -ra links <<< $data
   for i in "${!links[@]}"; do
-    [ $i -eq $amount ] && break
-    download_from_link_to_dir ${links[i]} $directory
+    [ $i -eq $DOWNLOAD_AMOUNT ] && break
+    download_from_link_to_dir ${links[i]} $DOWNLOAD_DIRECTORY
   done
 }
 
-if [ "$#" -lt 1 ]; then
-  echo "Missing parameter for download directory"
-  exit 1
-elif [ "$#" -lt 2 ]; then
-  DOWNLOAD_AMOUNT=65535
-fi
-DOWNLOAD_DIRECTORY=$1
-[ -z $DOWNLOAD_AMOUNT ] && DOWNLOAD_AMOUNT=$2
-check_installation_of_wget && parse_gradle_distributions_links_and_download $DOWNLOAD_DIRECTORY $DOWNLOAD_AMOUNT || (echo "wget is not installed" && exit 1)
+check_and_set_parameters "$@"
+check_installation_of_wget
+parse_gradle_distributions_links_and_download
