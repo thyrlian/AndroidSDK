9d914efc6ba45bc6443182d55347f558f6527c2a
HangoX <liang.hanguang93@gmail.com>
2019-01-23 10:17:29 +0800

    fix can not accept license when install platform28

--- android-sdk/license_accepter.sh
+++ android-sdk/license_accepter.sh
@@ -20,9 +20,11 @@ accept_all_android_licenses() {
     echo "Android licenses directory doesn't exist, creating one..."
     mkdir -p $ANDROID_LICENSES
   fi
-  accept_license_of android-sdk-license 8933bad161af4178b1185d1a37fbf41ea5269c55
-  accept_license_of android-sdk-license d56f5187479451eabf01fb78af6dfcb131a6481e
+  accept_license_of android-googletv-license 601085b94cd77f0b54ff86406957099ebe79c4d6、
+  accept_license_of android-sdk-license 24333f8a63b6825ea9c5514f83c2829b004d1fee
   accept_license_of android-sdk-preview-license 84831b9409646a918e30573bab4c9c91346d8abd
+  accept_license_of android-sdk-preview-license 504667f4c0de7af1a06de9f4b1727b84351f2910
+  accept_license_of google-gdk-license 33b6a2b64607f11b759f320ef9dff4ae5c47d97a
   accept_license_of intel-android-extra-license d975f751698a77b662f1254ddbeed3901e976f5a
 }
 
